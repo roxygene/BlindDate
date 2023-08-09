b82309777735694c03c425c6fe6b2443b6048e0e
Roksana <rgulewska@gmail.com>
2023-08-09 22:07:31 +0200

    Design changes New logo and color palette Changed buttons Finished starting layout

---
+++ app/src/main/java/com/roksanagulewska/blinddate/SignInActivity.kt
@@ -0,0 +1,15 @@
+package com.roksanagulewska.blinddate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        val etEmail = findViewById<EditText>(R.id.et_email)
+        val etPassword = findViewById<EditText>(R.id.et_password)
+    }
+}
